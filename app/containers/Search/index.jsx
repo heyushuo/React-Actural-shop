7d547ffe8cb055e47ebe9b5a53ed9649929786fa
heyushuo <1007561607@qq.com>
2018-03-16 00:18:12 +0800

    search

--- app/containers/Search/index.jsx
+++ app/containers/Search/index.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 
+import SearchHeader from '../../components/SearchHeader'
+import SearchList from './subpage/List'
+
 class Search extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
     }
-    componentDidMount(){
-        const req=this.props.params;
-        console.log(req)
-    }
     render() {
+        const params = this.props.params
         return (
             <div>
-                <h1>Search</h1>
+                <SearchHeader keyword={params.keyword}/>
+                <SearchList keyword={params.keyword} category={params.category}/>
             </div>
         )
     }
 }
 
-// 使用 require.ensure 异步加载，还不支持 ES6 的 export 
-// export default Search
-module.exports = Search
+export default Search
